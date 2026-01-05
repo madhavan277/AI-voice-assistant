a0f141c002421d50944720e83c6848ca990f750c
madhavan277 <139436593+madhavan277@users.noreply.github.com>
2026-01-05 16:55:40 +0530

    Update ai.js

--- frontend/src/ai.js
+++ frontend/src/ai.js
@@ -1,14 +1,17 @@
-import Vapi from "@vapi/web";
-
+import Vapi from "@vapi-ai/web";
 export const vapi = new Vapi(import.meta.env.VITE_VAPI_API_KEY);
-
+const assistantId = import.meta.env.VITE_ASSISTANT_ID;
 export const startAssistant = async (firstName, lastName, email, phone) => {
-  return await vapi.start({
-    assistantId: import.meta.env.VITE_ASSISTANT_ID,
-    metadata: { firstName, lastName, email, phone }
-  });
+    const assistantOverrides = {
+        variableValues: {
+            firstName,
+            lastName,
+            email,
+            phone
+        }
+    }
+    return await vapi.start(assistantId, assistantOverrides)
 };
-
 export const stopAssistant = () => {
-  vapi.stop();
-};
+    vapi.stop()
+}
